dda15a9af2c8f32c82fdddc7314ba9fbe5dda6d2
TsukasaCh <92731359+TsukasaCh@users.noreply.github.com>
2025-07-12 23:27:16 +0700

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,28 @@
+echo deleting old sshd config...
+
+rm -rf /etc/ssh/sshd_config
+
+sleep 1
+
+echo fetching new sshd config...
+
+wget https://raw.githubusercontent.com/TsukasaCh/RootSSH/refs/heads/main/sshd_config
+
+sleep 1
+
+echo copying authorized root keys to common user...
+
+cp /root/.ssh/authorized_keys /home/ubuntu/
+
+sleep 1
+
+echo changing permission keys...
+
+chmod 777 /home/ubuntu/authorized_keys
+
+sleep 1
+
+echo restarting sshd services...
+
+service ssh reload
+
